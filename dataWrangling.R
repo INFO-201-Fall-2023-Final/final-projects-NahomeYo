62b74ac03d33a363f2169df469f1b7d9c2889ade
NahomeYo <NahomeYo2002@gmail.com>
2023-11-28 23:55:59 -0800

    Merge final project

--- dataWrangling.R
+++ dataWrangling.R
@@ -7,31 +7,39 @@ library(readr)
 poverty_pse <- read_csv("C:/Users/nahom/final-projects-NahomeYo/poverty_pse.csv")
 Palestine_Body_Count <- read_csv("C:/Users/nahom/final-projects-NahomeYo/Palestine Body Count.csv")
 
-# Since the two columns representing years have different data types (character and double),
-# we convert the 'Year' column to match the other.
+# Convert the 'Year' columns to have consistent data types (character)
 poverty_pse$Year <- as.character(poverty_pse$Year)
 Palestine_Body_Count$Year <- as.character(Palestine_Body_Count$Year)
 
-# Joining the two datasets and creating one, unified dataset
+# Joining the two datasets and creating one, unified dataset based on the 'Year' column
 df <- merge(poverty_pse, Palestine_Body_Count, by = "Year")
 
-# Cleaning the unified dataset by removing unnecessary rows and columns
-df <- df[!is.na(df$Year), , drop = FALSE]
+# Convert "Value" column to numeric
+df <- mutate(df, Value = as.numeric(Value))
+
+# Cleaning the unified dataset by removing rows with missing 'Year' values
+df <- df[!is.na(df$Year) & !is.na(df$Year), ]
+
+# Removing unnecessary columns and rows
 df <- select(df, -c("Country ISO3", "Indicator Code", 'Palestinians Injuries', 'Israelis Injuries'))
 
-# Creating a new categorical variable called "Poverty_Level" that categorizes observations into different 
-# poverty levels based on the "Value" column, with "Medium Poverty" for values between 10 and 20,
-# "High Poverty" for values above 20, and NA for values below 10.
-df$Poverty_Level <- ifelse(df$Value >= 10 & df$Value <= 20, "Medium Poverty", NA)
-df$Poverty_Level <- ifelse(df$Value > 20, "High Poverty", df$Poverty_Level)
-df$Poverty_Level[is.na(df$Poverty_Level)] <- "Low Poverty"
-
+# Creating a new categorical variable called "Poverty_Level" based on the "Value" column
+# Categories: "low poverty" for values below 20, "medium poverty" for values between 20 and 40,
+# "high poverty" for values above 40
+df$Poverty_Level <- ifelse(df$Value > 40, "high", ifelse(df$Value > 20, "medium", "low"))
+df <- df[!is.na(df$Poverty_Level), ]
 
 # Creating a new continuous/numerical variable called "Total_Killed" that indicates
-# the total number of causalities of Palestinians and Israelis 
-df <- mutate(df, `Palestinians Killed` = as.numeric(`Palestinians Killed`), `Israelis Killed` = as.numeric(`Israelis Killed`)
-)
+# the total number of casualties of Palestinians and Israelis 
+df <- mutate(df, `Palestinians Killed` = as.numeric(`Palestinians Killed`), `Israelis Killed` = as.numeric(`Israelis Killed`))
 df <- mutate(df, Total_Killed = `Palestinians Killed` + `Israelis Killed`)
 
 # Creating at least one summarization data frame
-summarize_df <- summarise(group_by(df, Year), Average_Total_Killed = mean(Total_Killed))
+summarize_df <- summarise(group_by(df, Year, Poverty_Level), Average_Total_Killed = mean(Total_Killed))
+arranged_df <- arrange(summarize_df, Year, str_c(Average_Total_Killed))
+grouped_df <- group_by(arranged_df, Year)
+summarized_df <- summarise(
+  grouped_df,
+  Poverty_Level = str_c(Poverty_Level[1]),
+  Average_Total_Killed = as.numeric(Average_Total_Killed[1])
+)
